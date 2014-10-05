2b575a28e50a8db90cea4945518bcd119acc5806
Dmitrijs Sadovskis <sdo@inbox.lv>
2014-10-06 01:04:44 +0300

    window_upload ui

--- src/ui/upload/window_upload.c
+++ src/ui/upload/window_upload.c
@@ -27,7 +27,7 @@ static void initialise_ui(void) {
     s_res_gothic_18_bold = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
     // s_textlayer_1
     s_textlayer_1 = text_layer_create(GRect(2, 1, 141, 40));
-    text_layer_set_text(s_textlayer_1, "Please wait while workouts are uploaded.");
+    text_layer_set_text(s_textlayer_1, "Uploading workouts,\nplease wait...");
     text_layer_set_text_alignment(s_textlayer_1, GTextAlignmentCenter);
     text_layer_set_font(s_textlayer_1, s_res_gothic_18_bold);
     layer_add_child(window_get_root_layer(s_window), (Layer *) s_textlayer_1);
@@ -63,18 +63,23 @@ static void destroy_ui(void) {
 static void progress_bar_layer_update(ProgressBarLayer *bar, GContext *ctx) {
     ProgressData *data = (ProgressData *) layer_get_data(bar);
 
+    GRect bounds = layer_get_bounds(bar);
+    int16_t width_progress = (bounds.size.w * data->progress) / data->maximum;
+
     // Outline the progress bar
     graphics_context_set_stroke_color(ctx, GColorBlack);
-    GRect bounds = layer_get_bounds(bar);
     graphics_draw_round_rect(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), 4);
 
     // Fill the progress bar
     graphics_context_set_fill_color(ctx, GColorBlack);
-    graphics_fill_rect(ctx, GRect(0, 0, data->progress, bounds.size.h), 4, GCornersAll);
+    graphics_fill_rect(ctx, GRect(0, 0, width_progress, bounds.size.h), 4, GCornersAll);
 }
 
 static ProgressBarLayer *progress_bar_layer_create(GRect bounds, ProgressData initial_data) {
     ProgressBarLayer *progress_bar_layer = layer_create_with_data(bounds, sizeof(ProgressData));
+    ProgressData *data = (ProgressData *) layer_get_data(progress_bar_layer);
+    data->progress = initial_data.progress;
+    data->maximum = initial_data.maximum;
     layer_set_update_proc(progress_bar_layer, progress_bar_layer_update);
     layer_mark_dirty(progress_bar_layer);
 
@@ -95,9 +100,12 @@ static void handle_window_unload(Window *window) {
 
 void show_window_upload(void) {
     initialise_ui();
-    progress_bar_layer_1 = progress_bar_layer_create(GRect(6, 65, 130, 8), (ProgressData) {.progress = 0, .maximum = 12});
-    progress_bar_layer_2 = progress_bar_layer_create(GRect(6, 85, 130, 8), (ProgressData) {.progress = 0, .maximum = 12});
-    progress_bar_layer_3 = progress_bar_layer_create(GRect(6, 105, 130, 8), (ProgressData) {.progress = 0, .maximum = 12});
+    progress_bar_layer_1 = progress_bar_layer_create(GRect(6, 64, 130, 10), (ProgressData) {.progress = 7, .maximum = 12});
+    progress_bar_layer_2 = progress_bar_layer_create(GRect(6, 97, 130, 10), (ProgressData) {.progress = 2, .maximum = 12});
+    progress_bar_layer_3 = progress_bar_layer_create(GRect(6, 132, 130, 10), (ProgressData) {.progress = 9, .maximum = 12});
+    layer_add_child(window_get_root_layer(s_window), progress_bar_layer_1);
+    layer_add_child(window_get_root_layer(s_window), progress_bar_layer_2);
+    layer_add_child(window_get_root_layer(s_window), progress_bar_layer_3);
 
     window_set_window_handlers(s_window, (WindowHandlers) {
             .unload = handle_window_unload,
