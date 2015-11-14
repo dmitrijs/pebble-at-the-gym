e6b644531f137aae72380dc53d38c59c039b2019
Dmitrijs Sadovskis <sdo@inbox.lv>
2015-11-14 05:09:05 +0200

    move tests to a separate file

--- src/main.c
+++ src/main.c
@@ -1,45 +1,13 @@
 #include <pebble.h>
 #include "ui_gym/window_menu.h"
 #include "ui/window_error.h"
-
-static bool long_is_right_length() {
-//    APP_LOG(APP_LOG_LEVEL_DEBUG, "long size: %d", sizeof(long));
-//    APP_LOG(APP_LOG_LEVEL_DEBUG, "int size: %d", sizeof(int));
-//    APP_LOG(APP_LOG_LEVEL_DEBUG, "double size: %d", sizeof(double));
-//    APP_LOG(APP_LOG_LEVEL_DEBUG, "float size: %d", sizeof(float));
-
-    return sizeof(long) == 4;
-}
-
-static bool serialization_works() {
-    uint8_t buf[5];
-
-    long x = 1234567890;
-    buf[0] = (uint8_t) (x >> 24 % 256);
-    buf[1] = (uint8_t) (x >> 16 % 256);
-    buf[2] = (uint8_t) (x >> 8 % 256);
-    buf[3] = (uint8_t) (x >> 0 % 256);
-
-    uint8_t dst[5];
-    memcpy(dst, buf, 5);
-
-    long x2 = 0;
-    x2 += dst[0];
-    x2 <<= 8;
-    x2 += dst[1];
-    x2 <<= 8;
-    x2 += dst[2];
-    x2 <<= 8;
-    x2 += dst[3];
-
-    return x == x2;
-}
+#include "tests/tests.h"
 
 int main(void) {
-    if (serialization_works() && long_is_right_length()) {
+    if (tests_pass()) {
         show_window_menu();
     } else {
-        show_window_error("Auto-tests failed");
+        show_window_error("App tests failed.");
     }
 
     app_event_loop();
